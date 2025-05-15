2b1b2213569cd38fb066b0c81c0ac457240c0247
Bhanu Uday Sharma <118754034+SharmaUdayBhanu@users.noreply.github.com>
2025-05-15 15:47:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,5 @@ app.get('/', async (req, res) => {
 
 app.use('/articles', articleRouter)
 
-app.listen(5000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
